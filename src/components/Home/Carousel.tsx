b55d110606f1b1c619a39be42af100a5e6394d24
Dehemi Weerakkody <dehemiweerakkodi2@gmail.com>
2023-04-11 00:54:40 +0530

    Carousel changes

--- src/components/Home/Carousel.tsx
+++ src/components/Home/Carousel.tsx
@@ -1,64 +1,38 @@
 import React from "react";
 
 function Carousel() {
+  let items = new Array(4).fill(<></>);
+
   return (
-    <div className="carousel w-full h-2/6">
-      <div id="slide1" className="carousel-item relative w-full h-2/6">
-        <img
-          src="https://pharmaceutical-journal.com/wp-content/uploads/2021/01/herbal-medicines-ss-18-scaled.jpg"
-          className="w-full"
-        />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide4" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide2" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide2" className="carousel-item relative w-full h-2/6">
-        <img
-          src="https://pharmaceutical-journal.com/wp-content/uploads/2021/01/herbal-medicines-ss-18-scaled.jpg"
-          className="w-full"
-        />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide3" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide3" className="carousel-item relative w-full h-2/6">
-        <img
-          src="https://pharmaceutical-journal.com/wp-content/uploads/2021/01/herbal-medicines-ss-18-scaled.jpg"
-          className="w-full"
-        />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide4" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide4" className="carousel-item relative w-full h-2/6">
-        <img
-          src="https://pharmaceutical-journal.com/wp-content/uploads/2021/01/herbal-medicines-ss-18-scaled.jpg"
-          className="w-full"
-        />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide1" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
+    <div className="carousel w-full h-1/6">
+      {items.map((_, i) => {
+        return (
+          <div
+            key={i}
+            id={`slide${i + 1}`}
+            className="carousel-item relative w-full h-[40vh]"
+          >
+            <img
+              src="https://pharmaceutical-journal.com/wp-content/uploads/2021/01/herbal-medicines-ss-18-scaled.jpg"
+              className="w-full object-cover"
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a
+                href={i ? `#slide${i + 1}` : `#slide${items.length}`}
+                className="btn btn-circle"
+              >
+                ❮
+              </a>
+              <a
+                href={i + 1 === items.length ? `#slide1` : `#slide${i + 2}`}
+                className="btn btn-circle"
+              >
+                ❯
+              </a>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
